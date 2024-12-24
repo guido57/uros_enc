c38e031b57a9ac351d2711f9079d8d35d77343f9
guido57 <guido@dogsally.com>
2024-12-24 22:21:31 +0100

    RPlidarC1 added

--- src/main.cpp
+++ src/main.cpp
@@ -23,30 +23,24 @@
 #include "odometry.h"
 #include "ros2.h"
 #include "MotorController.h"
-#include "rpLidar.h"
-#include "rpLidarTypes.h"
+#include "RplidarC1.h"
 
 rcl_subscription_t subscriber;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 rclc_executor_t executor;
-rcl_timer_t timer;
-rcl_timer_t timer_scan;
-rcl_publisher_t scan_publisher;
+rcl_timer_t odom_pid_timer;
+rcl_timer_t lidar_timer;
+rcl_publisher_t lidar_publisher;
 std_msgs__msg__Int32 sub_msg;
 geometry_msgs__msg__Twist cmd_vel_msg;
 sensor_msgs__msg__LaserScan scan_msg;
 
 #define LED_PIN 2
 
-// lidar Parameters and lidar object
-#define SCAN_PUBLISH_INTERVAL 100  // Milliseconds
-const int MAX_SCAN_POINTS = 550;
-// rpLidar lidar(&Serial2, 460800);
-rpLidar * lidar;
-static float ranges[MAX_SCAN_POINTS];
-static float intensities[MAX_SCAN_POINTS];
+// Lidar object
+RplidarC1 lidar;
 
 // Left Motor PWM pins 
 # define PWM_LEFT_1_PIN 4
@@ -83,28 +77,31 @@ const int resolution = 8;
 MotorController leftWheel(PWM_LEFT_1_PIN, PWM_LEFT_2_PIN, tickPerRevolution_LW);
 MotorController rightWheel(PWM_RIGHT_1_PIN, PWM_RIGHT_2_PIN, tickPerRevolution_RW);
 
+float linearVelocity, angularVelocity;
+float vL, vR;
+float currentRpmL, currentRpmR;
+float currentmsL, currentmsR;
+float actuating_signal_LW, actuating_signal_RW;
 void motors_control(){
-float linearVelocity;
-  float angularVelocity;
   //linear velocity and angular velocity send cmd_vel topic
   linearVelocity  = cmd_vel_msg.linear.x;
   angularVelocity = cmd_vel_msg.angular.z;
   //linear and angular velocities are converted to leftwheel and rightwheel velocities
-  float vL = linearVelocity - angularVelocity * wheel_base / 2;
-  float vR = linearVelocity + angularVelocity * wheel_base / 2;
+  vL = linearVelocity - angularVelocity * wheel_base / 2;
+  vR = linearVelocity + angularVelocity * wheel_base / 2;
   //current wheel rpm is calculated
-  float currentRpmL = leftWheel.getRpm(enc_l_total);
-  float currentRpmR = rightWheel.getRpm(enc_r_total);
+  currentRpmL = leftWheel.getRpm(enc_l_total);
+  currentRpmR = rightWheel.getRpm(enc_r_total);
   // current wheel speed (m/s) is calculated
-  float currentmsL = currentRpmL * 2 * PI * wheel_radius / 60;
-  float currentmsR = currentRpmR * 2 * PI * wheel_radius / 60;
+  currentmsL = currentRpmL * 2 * PI * wheel_radius / 60;
+  currentmsR = currentRpmR * 2 * PI * wheel_radius / 60;
 
   //pid controlled is used for generating the pwm signal
-  float actuating_signal_LW = leftWheel.pid(vL, currentmsL);
-  float actuating_signal_RW = rightWheel.pid(vR, currentmsR);
-  printf("lv=%f av=%f vL=%f vR=%f msL=%f msR=%f LW=%f RW=%f                     \r",
-        linearVelocity, angularVelocity, vL, vR, currentmsL, currentmsR, actuating_signal_LW, actuating_signal_RW
-  );
+  actuating_signal_LW = leftWheel.pid(vL, currentmsL);
+  actuating_signal_RW = rightWheel.pid(vR, currentmsR);
+  // printf("lv=%.3f av=%.3f vL=%.2f vR=%.3f msL=%.3f msR=%.3f LW=%.0f RW=%f.0                     \r",
+  //       linearVelocity, angularVelocity, vL, vR, currentmsL, currentmsR, actuating_signal_LW, actuating_signal_RW
+  //);
   if (vL == 0 && vR == 0) { 
     leftWheel.stop(0,1);
     rightWheel.stop(2,3);
@@ -167,41 +164,48 @@ void motor_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   }
 }
 
-void publish_scan(int count) {
-  scan_msg.header.stamp.sec = millis() / 1000;
-  scan_msg.header.stamp.nanosec = (millis() % 1000) * 1000000;
+unsigned long total_loop_time = 0L;
+float loop_period = 0.0;
+void lidar_loop(){
 
-  scan_msg.angle_min = 0.0;
-  scan_msg.angle_max = 2 * M_PI;
-  scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / count;
-  scan_msg.range_min = 0.05;  // Minimum valid distance in meters
-  scan_msg.range_max = 12.0; // Maximum valid distance in meters
+    unsigned long uart_elapsed = millis();
+    int count = lidar.uartRx();
+    uart_elapsed = millis() - uart_elapsed;
 
-  for (int i = 0; i < count; i++) {
-    float angle = (lidar->DataBuffer[i].angle_high * 128 + lidar->DataBuffer[i].angle_low / 2) / 64.0 * DEG_TO_RAD;
-    int distance = lidar->DataBuffer[i].distance_high * 256 + lidar->DataBuffer[i].distance_low;
-    int quality = lidar->DataBuffer[i].quality / 4;
+    unsigned long process_elapsed = millis();
+    lidar.processFrame(count);
+    process_elapsed = millis()-process_elapsed;
 
-    // Store data in scan message
-    scan_msg.ranges.data[i] = (distance > 0) ? distance / 1000.0 : INFINITY;  // Convert mm to meters
-    scan_msg.intensities.data[i] = quality;
-  }
+    unsigned long publish_elapsed = millis();
+    rcl_ret_t ret_pub = rcl_publish(&lidar_publisher, &lidar.scan_msg, NULL);
+    publish_elapsed = millis() - publish_elapsed;
 
-  scan_msg.ranges.size = count;
-  scan_msg.intensities.size = count;
+    if(ret_pub != RCL_RET_OK){
+        printf("rcl_publish returned %d\r\n", ret_pub);
+        esp_restart();
+    }
 
-  // Publish the scan message
-  RCSOFTCHECK(rcl_publish(&scan_publisher, &scan_msg, NULL), "error publishing /scan");
+    // calculate loop period  
+    total_loop_time = millis()-total_loop_time;
+    float total_loop_time_f = (float) total_loop_time;
+    loop_period = loop_period*0.9 + total_loop_time_f*0.1;
+    
+    Serial.printf("lv=%.3f av=%.3f vL=%.2f vR=%.3f msL=%.3f msR=%.3f LW=%.0f RW=%.0f  %dpts:%lums-FrameProc:%lums-FramePub:%lums-LidarLoop:%lums-LidarFreq=%.1fHz-Serial2.available=%d\r\n",
+        linearVelocity, angularVelocity, vL, vR, currentmsL, currentmsR, actuating_signal_LW, actuating_signal_RW,
+        count, uart_elapsed, process_elapsed, publish_elapsed, total_loop_time, 1000.0/loop_period, Serial2.available()
+     );
+    total_loop_time = millis();
 }
 
-void scan_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+void lidar_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
   RCLC_UNUSED(last_call_time);
- if (timer != NULL )  {
+  if (timer != NULL )  {
 
-
-    //publish_scan(count); // Publish the scan data to /scan topic
-
+    unsigned long lidar_timer_cb_elapsed = millis(); 
+    lidar_loop();
+    // printf("\r\n%lu lidar_timer_callback took %lu millis. Serial2aval=%d\r\n", 
+    //       millis(), millis()-lidar_timer_cb_elapsed, Serial2.available());
   }
 }
 
@@ -217,8 +221,11 @@ void setup() {
   Serial.begin(115200);
   printf("setup ...\r\n");
   
-  lidar = new rpLidar(&Serial2, 460800);
-
+  lidar.begin();
+  delay(1000);
+  lidar.resetLidar();
+  delay(800);
+  lidar.startLidar();
   my_set_microros_wifi_transports();
   
   pinMode(LED_PIN, OUTPUT);
@@ -269,42 +276,22 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry),
     "/odom"), "rclc_publisher_init_default /odom error!");
 
-  // Create publisher
+  // Create /scan publisher
+  printf("create /scan publisher...\r\n");
   RCCHECK(rclc_publisher_init_default(
-    &scan_publisher,
+    &lidar_publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
     "/scan"),"rclc_publisher_init_best_effort /scan error!");
-
-  // Initialize scan message
-  scan_msg.header.frame_id.data = (char *)"lidar_frame";
-  scan_msg.header.frame_id.size = strlen(scan_msg.header.frame_id.data);
-  scan_msg.header.frame_id.capacity = scan_msg.header.frame_id.size + 1;
-  scan_msg.ranges.capacity = MAX_SCAN_POINTS;
-  scan_msg.ranges.data = ranges;
-  scan_msg.intensities.capacity = MAX_SCAN_POINTS;
-  scan_msg.intensities.data = intensities;
-
-  // Setup lidar
-  lidar->resetDevice(); // Reset the device to ensure good status
-  stDeviceStatus_t sdst = lidar->getDeviceHealth();
-  Serial.printf("sdst.errorCode_high=%d  sdst.errorCode_low=%d sdst.status=%d\r\n", sdst.errorCode_high, sdst.errorCode_low, sdst.status);
-
-  //lidar.setAngleOfInterest(0, 360); // Set field of view
-  bool ret = lidar->start(standard); // Start standard scan
-  if (ret)
-    Serial.println("rplidar C1 started correctly!");
-  else
-    Serial.println("Error starting rplidar C1");
   
   // create 100 msecs timer
   printf("Create /scan timer...\r\n");
-  const unsigned int timer_scan_timeout = 100;
+  const unsigned int timer_scan_timeout = 70;
   RCCHECK(rclc_timer_init_default(
-    &timer_scan,
+    &lidar_timer,
     &support,
     RCL_MS_TO_NS(timer_scan_timeout),
-    scan_timer_callback),"rclc_timer_init_default scan_timer_callback error!");
+    lidar_timer_callback),"rclc_timer_init_default scan_timer_callback error!");
 
   // Initialize subscriber
   printf("create /cmd_vel subscriber...\r\n");
@@ -323,15 +310,15 @@ void setup() {
   printf("create motor timer...\r\n");
   const unsigned int timer_timeout = 20;
   RCCHECK(rclc_timer_init_default(
-    &timer,
+    &odom_pid_timer,
     &support,
     RCL_MS_TO_NS(timer_timeout),
     motor_timer_callback),"rclc_timer_init_default error!");
 
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator),"rclc_executor_init error");
-  RCCHECK(rclc_executor_add_timer(&executor, &timer),"rclc_executor_add_timer motor error");
-  RCCHECK(rclc_executor_add_timer(&executor, &timer_scan),"rclc_executor_add_timer scan error");
+  RCCHECK(rclc_executor_add_timer(&executor, &odom_pid_timer),"rclc_executor_add_timer motor error");
+  RCCHECK(rclc_executor_add_timer(&executor, &lidar_timer),"rclc_executor_add_timer scan error");
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &cmd_vel_msg, sub_cmd_vel_callback, ON_NEW_DATA),"rclc_executor_add_subscription error!");
 
   printf("end of setup!\r\n");
